26f4d28ea1066ceca2d902afa44dfe2c5744cf61
rikikudohust <trunghieubh0102@gmail.com>
2022-10-04 00:41:04 +0700

    Manual merge

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 
-<<<<<<< HEAD
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -16,22 +15,3 @@ contract TestToken is ERC20, Ownable {
         _mint(msg.sender, 100000000000);
     }
 }
-=======
-pragma solidity ^0.8.10;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-/**
-* @title TestToken is a basic ERC20 Token
-*/
-contract TestToken is ERC20, Ownable{
-
-/**
-    * @dev assign totalSupply to account creating this contract */
-    constructor() ERC20("TestToken", "TT") {
-
-        _mint(msg.sender, 100000000000);
-
-    }
-}
->>>>>>> proof
