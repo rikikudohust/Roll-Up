c279191d8c3e26e832ecf8eb00d77498012e4c35
rikikudohust <trunghieubh0102@gmail.com>
2022-10-17 15:44:22 +0700

    merge withdraw feature

--- scripts/config/configuration.js
+++ scripts/config/configuration.js
@@ -1,6 +1,6 @@
 contract = {
     merkle: "0xA7E3983545F35678bc37dd5789a8B3FaeF529afE",
-    rollup: "0x88682072A36DF6d93875A926C9bb853f9F10E6eB"
+    rollup: "0x3cde180D5166b701a17a884db5eE014291F7b949"
 }
 
 module.exports = {
